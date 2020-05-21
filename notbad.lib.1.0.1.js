7d921d189f5afb4ef56c429d5b3b423ac174f06f
Zhenya <jen9.vovchyk@outlook.com>
2020-05-21 21:11:07 +0300

    Update notbad.lib.1.0.1.js

--- notbad.lib.1.0.1.js
+++ notbad.lib.1.0.1.js
@@ -4,14 +4,14 @@ const dir = (x) => console.dir(x);
 const dirXml = (x) => this.console.dirxml(x);
 const group = (x) => console.group(x);
 const groupEnd = (x) => console.groupEnd(x);
-const timer = (x) => console.time(x);
+const timer = (x) => console.timer(x);
 const timerEnd = (x) => console.timeEnd(x);
 const trace = () => console.trace();
 
 const win = window;
 const doc = document;
-const qs = (x) => document.querySelector(x);
-const qsAll = (x) => document.querySelectorAll(x);
+const js = (x) => document.querySelector(x);
+const jsAll = (x) => document.querySelectorAll(x);
 const animOn = (x) => requestAnimationFrame(x);
 const animOff = (x) => requestAnimationFrame(x);
 
